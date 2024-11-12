c3564585da2a507b12e5ae184b8eb0eb55b5383c
Aleksa Petrovic <aleksapetrovic2907@gmail.com>
2024-11-13 00:21:36 +0100

    convert cell to ES2015 class

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 const EMPTY_CELL = undefined;
 
-function Cell(row, col, value = EMPTY_CELL) {
-    this.row = row;
-    this.col = col;
-    this.value = value;
-}
+class Cell {
+    constructor(row, col, value = EMPTY_CELL) {
+        this.row = row;
+        this.col = col;
+        this.value = value;
+    }
 
-Cell.prototype.isEmpty = function() { 
-    return this.value === EMPTY_CELL;
-}
+    isEmpty() {
+        return this.value === EMPTY_CELL;
+    }
 
-Cell.prototype.isOccupied = function() { 
-    return this.value !== EMPTY_CELL;
+    isOccupied() {
+        return this.value !== EMPTY_CELL;
+    }
 }
