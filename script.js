dc59be32142760e6a0c9a87d7f347f425e8159eb
Aleksa Petrovic <aleksapetrovic2907@gmail.com>
2024-11-13 00:07:34 +0100

    implement methods isEmpty & isOccupied to Cell prototype

--- script.js
+++ script.js
@@ -5,3 +5,11 @@ function Cell(row, col, value = EMPTY_CELL) {
     this.col = col;
     this.value = value;
 }
+
+Cell.prototype.isEmpty = function() { 
+    return this.value === EMPTY_CELL;
+}
+
+Cell.prototype.isOccupied = function() { 
+    return this.value !== EMPTY_CELL;
+}
