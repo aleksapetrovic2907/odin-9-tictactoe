357e6469526c99d1c9855ca8ff838376555eab8d
aleksapetrovic2907 <aleksa.petrovic2907@gmail.com>
2024-11-13 01:09:58 +0100

    implement Game class and its constructor

--- script.js
+++ script.js
@@ -47,3 +47,12 @@ class Player {
         this.token = token;
     }
 }
+
+class Game {
+    constructor(player1, player2) {
+        this.player1 = player1;
+        this.player2 = player2;
+        this.board = new Board(3, 3);
+        this.turn = this.player1;
+    }
+}
